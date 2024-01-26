2dc35bd0cc4f7290dd80a32d4b54e86ab40c7028
Liam Andersson <liaan514@student.liu.se>
2024-01-26 21:23:28 +0100

    Friday

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,20 +1,54 @@
 import 'package:flutter/material.dart';
+import 'optionpage.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MaterialApp(
+      home: const HomePage(), 
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: "MontserratBold"
+      )
+    )
+  );
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
+    return Scaffold(
         body: Center(
-          child: Text('Test test!'),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  Image.asset("images/placeholder.png"),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      shadowColor: Colors.grey,
+                      textStyle: const TextStyle(
+                        color: Colors.black,
+                      ) 
+                    ),
+                    onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const OptionPage()));
+                    
+                    },
+                    child: const Text('Start')
+                  )
+                ]
+              )
+            ),
+          ]
+        )          
         ),
-      ),
-    );
+      );
   }
 }
